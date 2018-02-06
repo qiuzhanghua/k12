64c9c6e723d7d10c554a28271ba9a44342e60d02
邱张华 <qiuzh@mail.taiji.com.cn>
2018-02-06 10:33:36 +0800

    upgrade Spring Cloud Data Flow to 1.3

--- README.md
+++ README.md
@@ -1,40 +1,53 @@
-质数的计算
-==
+# 质数的计算
+
 基本的方法
 
-Spring Cloud Stream Apps属性文件转换
-==
+## Spring Cloud Stream Apps属性文件转换
 
-将sink.file=http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/file-sink-kafka-10/1.2.0.RELEASE/file-sink-kafka-10-1.2.0.RELEASE.jar
 
-转换为 maven:org.springframework.cloud.stream.app:file-sink-kafka-10:1.2.0.RELEASE
+将sink.file=http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/file-sink-kafka-10/1.3.1.RELEASE/file-sink-kafka-10-1.3.1.RELEASE.jar
+
+转换为 maven:org.springframework.cloud.stream.app:file-sink-kafka-10:1.3.1.RELEASE
 
 文件来源 http://cloud.spring.io/spring-cloud-stream-app-starters/
 
 
-Spring Cloud Data Flow的基本使用
-===
+### Spring Cloud Data Flow的基本使用
 
+```bash
 cd /opt/kafka
 
 bin/zookeeper-server-start.sh config/zookeeper.properties
 
 bin/kafka-server-start.sh config/server.properties
 
-
 bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic output --from-beginning
 
 cd /opt
 
-java -jar spring-cloud-dataflow-server-local-1.2.3.RELEASE.jar --maven.remote-repositories.repo1.url=http://repo.taiji.com.cn:8081/repository/maven-public/
+java -jar spring-cloud-dataflow-server-local-1.3.0.RELEASE.jar --maven.remote-repositories.nexus.url=http://repo.taiji.com.cn:8081/repository/maven-public/
+```
+or create folder config under which add application.properties with 
+
+```properties
+maven.remote-repositories.nexus.url=http://repo.taiji.com.cn:8081/repository/maven-public/
+```
+and run
+```bash
+java -jar spring-cloud-dataflow-server-local-1.3.0.RELEASE.jar
 
 cd /opt
 
-java -jar spring-cloud-dataflow-shell-1.2.3.RELEASE.jar
+java -jar spring-cloud-dataflow-shell-1.3.0.RELEASE.jar
+```
+```
+app register --name time --type source --uri maven:org.springframework.cloud.stream.app:time-source-kafka-10:1.3.1.RELEASE
 
-app register --name time --type source --uri maven:org.springframework.cloud.stream.app:time-source-kafka-10:1.2.0.RELEASE
-
-app register --name log  --type sink   --uri maven:org.springframework.cloud.stream.app:log-sink-kafka-10:1.2.0.RELEASE
+app register --name log  --type sink   --uri maven:org.springframework.cloud.stream.app:log-sink-kafka-10:1.3.1.RELEASE
 
 
 app import file:kafka-10.properties
+```
+
+## 2018/02/06
+Upgrade version to 1.3.0
--- src/main/kotlin/com/example/k12/K12Application.kt
+++ src/main/kotlin/com/example/k12/K12Application.kt
@@ -1,5 +1,7 @@
 package com.example.k12
 
+import com.hankcs.hanlp.HanLP
+import com.hankcs.hanlp.tokenizer.StandardTokenizer
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import java.io.FileOutputStream
@@ -9,6 +11,25 @@ import java.net.URL
 import java.util.*
 import kotlin.collections.HashMap
 
+import edu.stanford.nlp.ling.CoreAnnotations
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation
+import edu.stanford.nlp.ling.CoreLabel
+import edu.stanford.nlp.pipeline.Annotation
+import edu.stanford.nlp.pipeline.StanfordCoreNLP
+import edu.stanford.nlp.util.CoreMap;
+import java.io.PrintWriter
+import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation
+import com.hankcs.hanlp.tokenizer.NLPTokenizer
+import com.hankcs.hanlp.tokenizer.IndexTokenizer
+
+
+
+
+
+
+
 @SpringBootApplication
 class K12Application
 
@@ -16,28 +37,28 @@ fun main(args: Array<String>) {
   SpringApplication.run(K12Application::class.java, *args)
 
   // teach my daughter prime numbers
-  var primes = BooleanArray(1000, { _ -> true });
-  primes[0] = false;
-  primes[1] = false;
-  for (i in 2..sqrt(primes.size * 1.0).toInt()) {
-    if (primes[i]) {
-      var j = 2
-      while (i * j < primes.size) {
-        primes[i * j] = false
-        j++
-      }
-    }
-  }
-  for (i in 2..100) {
-    if (primes[i]) {
-      print(i)
-      print(" ")
-    }
-  }
-  println()
-  println(primes[157])
-  println(primes[191])
-
+//  var primes = BooleanArray(1000, { _ -> true });
+//  primes[0] = false;
+//  primes[1] = false;
+//  for (i in 2..sqrt(primes.size * 1.0).toInt()) {
+//    if (primes[i]) {
+//      var j = 2
+//      while (i * j < primes.size) {
+//        primes[i * j] = false
+//        j++
+//      }
+//    }
+//  }
+//  for (i in 2..100) {
+//    if (primes[i]) {
+//      print(i)
+//      print(" ")
+//    }
+//  }
+//  println()
+//  println(primes[157])
+//  println(primes[191])
+//
 
   // convert
   //   sink.file=http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/file-sink-kafka-10/1.2.0.RELEASE/file-sink-kafka-10-1.2.0.RELEASE.jar
@@ -47,9 +68,14 @@ fun main(args: Array<String>) {
   // http://cloud.spring.io/spring-cloud-stream-app-starters/
 
   val map = HashMap<String, String>()
-  map.put("rabbit", "http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/spring-cloud-stream-app-descriptor/Bacon.RELEASE/spring-cloud-stream-app-descriptor-Bacon.RELEASE.rabbit-apps-maven-repo-url.properties")
-  map.put("kafka-09", "http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/spring-cloud-stream-app-descriptor/Bacon.RELEASE/spring-cloud-stream-app-descriptor-Bacon.RELEASE.kafka-09-apps-maven-repo-url.properties")
-  map.put("kafka-10", "http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/spring-cloud-stream-app-descriptor/Bacon.RELEASE/spring-cloud-stream-app-descriptor-Bacon.RELEASE.kafka-10-apps-maven-repo-url.properties")
+  // 1.2.x
+//  map.put("rabbit", "http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/spring-cloud-stream-app-descriptor/Bacon.RELEASE/spring-cloud-stream-app-descriptor-Bacon.RELEASE.rabbit-apps-maven-repo-url.properties")
+//  map.put("kafka-09", "http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/spring-cloud-stream-app-descriptor/Bacon.RELEASE/spring-cloud-stream-app-descriptor-Bacon.RELEASE.kafka-09-apps-maven-repo-url.properties")
+//  map.put("kafka-10", "http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/spring-cloud-stream-app-descriptor/Bacon.RELEASE/spring-cloud-stream-app-descriptor-Bacon.RELEASE.kafka-10-apps-maven-repo-url.properties")
+
+  // 1.3.x
+  map.put("rabbit", "http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/spring-cloud-stream-app-descriptor/Celsius.SR1/spring-cloud-stream-app-descriptor-Celsius.SR1.rabbit-apps-maven-repo-url.properties")
+  map.put("kafka-10", "http://repo.spring.io/libs-release/org/springframework/cloud/stream/app/spring-cloud-stream-app-descriptor/Celsius.SR1/spring-cloud-stream-app-descriptor-Celsius.SR1.kafka-10-apps-maven-repo-url.properties")
 
   for (key in map.keys) {
     val url = map.get(key)
@@ -61,24 +87,79 @@ fun main(args: Array<String>) {
 
     prop1.load(connection.inputStream)
     for (name in prop1.stringPropertyNames()) {
-      val v: String = prop1.getProperty(name).drop("http://repo.spring.io/libs-release/".length);
+      val v: String = prop1.getProperty(name).drop("http://repo.spring.io/libs-release/".length - 5);
+//      println(v)
       val ss = v.split("/");
+//      println(ss)
+
       val sb = StringBuilder()
-      sb.append("maven:")
+      val sb2 = StringBuilder()
+
+//      sb.append("maven:")
       for (i in 0..ss.lastIndex - 3) {
         sb.append(ss[i])
         sb.append(".")
       }
       sb.deleteCharAt(sb.lastIndex)
+      sb2.append("<dependency><groupId>")
+      sb2.append(sb.toString())
+      sb2.append("</groupId>")
+      sb2.append("<artifactId>")
+      sb2.append(ss[ss.lastIndex - 2])
+      sb2.append("</artifactId>")
+      sb2.append("<version>")
+      sb2.append(ss[ss.lastIndex - 1])
+      sb2.append("</version></dependency>")
+      println(sb2.toString())
+
       sb.append(":")
       sb.append(ss[ss.lastIndex - 2])
       sb.append(":")
       sb.append(ss[ss.lastIndex - 1])
+      sb.insert(0, "maven:")
       prop0.put(name, sb.toString())
     }
     val output = FileOutputStream(key + ".properties");
     prop0.store(output, "");
     connection.disconnect()
     output.close()
+
+  }
+
+//  val props = Properties()
+//  props.setProperty("annotators",
+//      "tokenize, ssplit, pos, lemma, ner, parse")
+//  val pipeline = StanfordCoreNLP("CoreNLP-chinese.properties")
+//  val annotation = Annotation(
+//      "This is a short sentence。 And this is another。 我爱北京天安门，天安门上太阳升。")
+//  pipeline.annotate(annotation);
+//  val sentences = annotation.get(
+//      CoreAnnotations.SentencesAnnotation::class.java)
+//    if (sentences != null && sentences.size > 0) {
+//      for(i in sentences.indices) {
+//        val tree = sentences[i].get(TreeAnnotation::class.java)
+//        val out = PrintWriter(System.out)
+//        out.println("The $i sentence parsed is:")
+//        tree.pennPrint(out)
+//        val tokens = sentences[i].get(CoreAnnotations.TokensAnnotation::class.java)
+//        println("字/词" + "\t " + "词性" + "\t " + "实体标记")
+//        println("-----------------------------")
+//        for (token in tokens) {
+//          val word = token.getString(TextAnnotation::class.java)
+//          val pos = token.getString(PartOfSpeechAnnotation::class.java)
+//          val ner = token.getString(NamedEntityTagAnnotation::class.java)
+//          println("$word\t $pos\t $ner")
+//        }
+//      }
+//    }
+
+  println(HanLP.segment("你好，欢迎使用HanLP汉语处理包！"));
+  val termList = StandardTokenizer.segment("商品和服务")
+  println(termList)
+  val termList2 = NLPTokenizer.segment("中国科学院计算技术研究所的宗成庆教授正在教授自然语言处理课程")
+  println(termList2)
+  val termList3 = IndexTokenizer.segment("主副食品")
+  for (term in termList3) {
+    println(term.toString() + " [" + term.offset + ":" + (term.offset + term.word.length) + "]")
   }
 }
